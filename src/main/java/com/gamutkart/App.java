c402fef6f18d0a1ef19827feb0fb0abf52cfd8a8
sree <konduru.blr@gmail.com>
2019-05-18 10:56:45 +0530

    added text

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-// testing continuous buids in Jenkins
+// testing continuous buids in Jenkins for every 2 minuts
 		for(i=0;i<=40;i++)
 		{
 			i += 3;
